21a38571a9f10516df822d186e8ea03ad879f58d
ifermon <github-ifermon@sneakemail.com>
2020-03-02 11:08:18 -0500

    Working with links in related business objects.

--- generate_html_files.py
+++ generate_html_files.py
@@ -19,7 +19,7 @@ from string import Template
 
 __author__ = "Ivan Fermon"
 __copyright__ = "Copyright 2020"
-__credits__ = ["None",]
+__credits__ = ["None", ]
 __license__ = "MIT"
 __maintainer__ = "Ivan Fermon"
 __version__ = ".9"
@@ -47,18 +47,25 @@ print(sys.version)  # We need to be on 3.7+
         - options - this is the same for all files
         - table_rows - this is file specific
 """
-
-html_template_path = "html_template.html"
-distribution_dir = "./bus_objs"
+HTML_SELECT_ONLY = True
+html_table_template_path = "html_table_template.html"
+html_select_template_path = "html_select_template.html"
+object_distribution_dir = "./dist"
+index_distribution_dir = "./dist"
 csv_file_path = "All_Fields-Reduced.csv"  # Load the field definitions
 
 #  allows me to add simple attributes to lists like file name
+
+
 class List_w_Attr(list):
     pass
 
 # Default template uses '$' which for clear reasons in an html template won't work
+
+
 class My_Template(Template):
     delimiter = '&'
+
 
 data = []
 data_by_bus_obj = defaultdict(List_w_Attr)
@@ -73,45 +80,66 @@ data_by_bus_obj = defaultdict(List_w_Attr)
 headers_wo_spaces = []
 with open(csv_file_path) as csv_file:
     header_reader = csv.reader(csv_file)
-    header_reader.__next__() # skip the first row
+    header_reader.__next__()  # skip the first row
     headers = header_reader.__next__()
     for h in headers:
         # Replace spaces with _'s or the column filtering won't work
-        headers_wo_spaces.append(h.translate(str.maketrans(" ","_")))
+        headers_wo_spaces.append(h.translate(str.maketrans(" ", "_")))
 
 """ Read the CSV file  and generate the data """
 with open(csv_file_path) as csv_file:
     csv_reader = csv.DictReader(csv_file, headers_wo_spaces)
-    csv_reader.__next__() # throw away the first row
-    csv_reader.__next__() # throw away the second row
+    csv_reader.__next__()  # throw away the first row
+    csv_reader.__next__()  # throw away the second row
     for row in csv_reader:
         data.append(row)
-        fname_transformation = row['Business_Object'].maketrans(" /", "_-", "()") # trans table to remove illegal chars 
+        fname_transformation = row['Business_Object'].maketrans(
+            " /", "_-", "()")  # trans table to remove illegal chars
         data_by_bus_obj[row['Business_Object']].append(row)
-        data_by_bus_obj[row['Business_Object']].fname = row['Business_Object'].translate(fname_transformation)
+        data_by_bus_obj[row['Business_Object']].fname = row['Business_Object'].translate(
+            fname_transformation)
     headers = row.keys()
 
-""" Load the html template """
-with open(html_template_path) as tmplate_file:
-    t = My_Template(tmplate_file.read())
+""" Load the html templates """
+with open(html_table_template_path) as tmplate_file:
+    table_template = My_Template(tmplate_file.read())
+with open(html_select_template_path) as tmplate_file:
+    index_template = My_Template(tmplate_file.read())
 
 """ Now all my data is loaded from the source file, start generating the include strings """
 
 """ Construct the html for the options """
-option_str = ""
+option_str = ""  # javacript bootstrap-select formatting
+option2_str = ""  # no javacript html5 only input datalist formatting
 for key, value in data_by_bus_obj.items():
     option_str += '<option id="{0}">{1}</option>\n'.format(value.fname, key)
+    option2_str += '<option id="./{0}.html" value="{1}" />'.format(
+        value.fname, key)
 
 """ 
     Now generate the files
 """
+ctr = 0
 for key, value in data_by_bus_obj.items():
+    for r in value:
+        v = r['Related_Business_Object_Name']
+        if v in data_by_bus_obj:
+            fname = data_by_bus_obj[v].fname
+            r['Related_Business_Object_Name'] = '<a href="{0}.html">{1}</a>'.format(fname, v)
+        # sys.exit(0)
     substitute_dict = {
         'header_title': key,
         'table_title': key,
         'options': option_str,
         'table_rows': json.dumps(value)
     }
-    with open("{0}/{1}.html".format(distribution_dir, value.fname), "w") as f:
-        f.write(t.substitute(substitute_dict))
-
+    if (HTML_SELECT_ONLY):
+        with open("{0}/{1}.html".format(object_distribution_dir, value.fname), "w") as f:
+            f.write(table_template.substitute(substitute_dict))
+        with open("{0}/selector.html".format(index_distribution_dir), "w") as f:
+            f.write(index_template.substitute(substitute_dict))
+    else:
+        with open("{0}/{1}.html".format(object_distribution_dir, value.fname), "w") as f:
+            f.write(table_template.substitute(substitute_dict))
+        with open("{0}/index.html".format(index_distribution_dir), "w") as f:
+            f.write(index_template.substitute(substitute_dict))
